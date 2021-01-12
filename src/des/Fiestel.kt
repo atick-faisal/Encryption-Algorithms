0475eb20c90e1ce93999a263ef361370fd4871d5
Atick Faisal <atickfaisal@gmail.com>
2021-01-12 21:00:59 +0600

    sboxes done

--- src/des/Fiestel.kt
+++ src/des/Fiestel.kt
@@ -2,6 +2,10 @@ package des
 
 import des.Config.EP
 import des.Config.P32
+import des.Config.SBOXES
+import sdes.Config
+import sdes.Fiestel
+import kotlin.math.pow
 
 object Fiestel {
     private fun expandPermutation(x: ULong): ULong {
@@ -20,6 +24,23 @@ object Fiestel {
             val currentBit = (x shr (4 - p)) and 1UL
             val shiftedX = currentBit shl (3 - index)
             result += shiftedX
+        }
+        return result
+    }
+
+    private fun getSValueAt(x: Int, sBox: Array<IntArray>): ULong {
+        val row = ((x and 0b100000) shr 2) or (x and 0b000001)
+        val col = (x shr 1) and 0b011110
+        return sBox[row][col].toULong()
+    }
+
+    private fun applySBoxes(x: ULong): ULong {
+        var result = 0UL
+        val mask: ULong = (2.0.pow(6)).toULong() - 1UL
+        for (i in 1..8) {
+            val temp = (x shr ((8 - i) * 6)) and mask
+            val value = getSValueAt(temp.toInt(), SBOXES[i - 1])
+            result = (result shl ((i - 1) * 6)) or value
         }
         return result
     }
