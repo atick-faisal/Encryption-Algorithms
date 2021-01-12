05c7d9933653c3d344fccdfbfcf2f735ac93717d
Atick Faisal <atickfaisal@gmail.com>
2021-01-12 12:06:27 +0600

    inverse permutation done

--- src/sdes/Util.kt
+++ src/sdes/Util.kt
@@ -2,9 +2,27 @@ package sdes
 
 import sdes.Config.IP
 
+fun generateInverseIP(ip: List<Int>): List<Int> {
+    val inverseIp = mutableListOf<Int>()
+    for (i in 1..ip.size)
+        inverseIp.add(ip.indexOf(i) + 1)
+    return inverseIp
+}
+
 fun initialPermutation(x: Int): Int {
     var result = 0
     IP.forEachIndexed { index, p ->
+        val currentBit = (x shr (8 - p)) and 1
+        val shiftedX = currentBit shl (7 - index)
+        result += shiftedX
+    }
+    return result
+}
+
+fun inversePermutation(x: Int): Int {
+    var result = 0
+    val inverseIp = generateInverseIP(IP)
+    inverseIp.forEachIndexed { index, p ->
         val currentBit = (x shr (8 - p)) and 1
         val shiftedX = currentBit shl (7 - index)
         result += shiftedX
