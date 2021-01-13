1839b074d9a2867cdcc1bbdbb621f7ba9b175bb7
Atick Faisal <atickfaisal@gmail.com>
2021-01-13 16:27:36 +0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Encryption_Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Encryption_Algorithms.dir/main.c.o"
+  "Encryption_Algorithms"
+  "Encryption_Algorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Encryption_Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
