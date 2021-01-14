a315735e866b825991adc31f6e67e5e37c9148b9
Atick Faisal <atickfaisal@gmail.com>
2021-01-14 14:36:21 +0600

    sdes util

--- sdes/sdes_util.c
+++ sdes/sdes_util.c
@@ -23,3 +23,20 @@ int initial_permutation(int x, const int* ip) {
     }
     return result;
 }
+
+int inverse_initial_permutation(int x, const int* inverse_ip) {
+    int result = 0;
+    for (int i = 0; i < IP_LENGTH; i++) {
+        int current_bit = x >> (IP_LENGTH - inverse_ip[i]) & 1;
+        int shifted_bit = current_bit << (IP_LENGTH - (i + 1));
+        result += shifted_bit;
+    }
+    return result;
+}
+
+int switch_order(int x, size_t size) {
+    size_t shift_amount = size / 2;
+    int msb = x << (shift_amount);
+    int lsb = x & ((1 << shift_amount) - 1);
+    return (lsb << shift_amount) | msb;
+}
